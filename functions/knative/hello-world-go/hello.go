72140fad911c4d35f5330a0641d7340d533ce4c8
loheagn <loheagn@icloud.com>
2022-05-18 20:12:49 +0800

    more log in hell.go

--- functions/knative/hello-world-go/hello.go
+++ functions/knative/hello-world-go/hello.go
@@ -1,10 +1,23 @@
 package hello
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
+	hostname, _ := os.Hostname()
+	fmt.Fprintf(w, "%s\t, Hello, %s!\n", hostname, r.URL.Path[1:])
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	defer cancel()
+	go func() {
+		for {
+			fmt.Printf("Hello, %s!\n\n", hostname)
+			time.Sleep(3 * time.Second)
+		}
+	}()
+	<-ctx.Done()
 }
